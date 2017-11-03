fda9db1bb30db4af3195e237a9694c41bd666ca2
dilipMaharjan <dilipmaharjann@gmail.com>
2017-11-03 16:42:07 -0400

    crud methods in functional views and url pattern

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def post_create(request):
+    return HttpResponse("Post Create")
+
+
+def post_detail(request):
+    return HttpResponse("Detail")
+
+
+def post_list(request):
+    return HttpResponse("List")
+
+
+def post_update(request):
+    return HttpResponse("Update")
+
+
+def post_delete(request):
+    return HttpResponse("Delete")
